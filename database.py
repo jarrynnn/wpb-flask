9e926a8524edc1455327290f9b4cf3b668a08843
jarrynnn <32430517+jarrynnn@users.noreply.github.com>
2019-07-07 11:49:17 +0100

    playing around with initial d/b and html

--- database.py
+++ database.py
@@ -18,8 +18,9 @@ def init_db(create_test_data=False):
     if (create_test_data):
         from database import db_session
 
-        db_session.add(models.Country(name='UK', stat=100, colour="3e95cd"))
-        db_session.add(models.Country(name='China', stat=17, colour="8e5ea2"))
-        db_session.add(models.Country(name='USA', stat=198, colour="3cba9f"))
+        db_session.add(models.Country(name='UK', stat=100, colour="3e95cd", background="3e7dcd"))
+        db_session.add(models.Country(name='China', stat=17, colour="8e5ea2", background="3e7dcd"))
+        db_session.add(models.Country(name='USA', stat=198, colour="3cba9f", background="3e7dcd"))
+        db_session.add(models.Country(name='France', stat=200, colour="cd3e95", background="3e7dcd"))
 
         db_session.commit()
--- models.py
+++ models.py
@@ -8,10 +8,11 @@ class Country(Base):
     stat = Column(Integer, nullable=False)
     colour = Column(String(6), nullable=False)
 
-    def __init__(self, name=None, stat=0, colour="000000"):
+    def __init__(self, name=None, stat=0, colour="000000", background="000000"):
         self.name = name
         self.stat = stat
         self.colour = colour
+        self.background = background
 
     def __repr__(self):
         return '%r (%s)' % (self.name, self.stat)
